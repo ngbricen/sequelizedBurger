445fa30d383c08cc58449b2d5fc7c8ac2dcd13a8
ngbricen <bricen@gmail.com>
2017-08-21 22:33:48 -0400

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Double Up', 0);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Cool Beans',0);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Peanut Dream',0);
