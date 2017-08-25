f2cfeeea155aea3b9468f6a83f1cf09c3f77e494
ngbricen <bricen@gmail.com>
2017-08-25 16:45:45 -0400

    Adjusting connection string

--- config/connection.js
+++ config/connection.js
@@ -6,27 +6,21 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-// if (process.env.JAWSDB_URL){ 
-//   var sequelize = new Sequelize(process.env.JAWSDB_URL);
-// }
-// else { 
+if (process.env.JAWSDB_URL){ 
+  var sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else { 
   var sequelize = new Sequelize("zvrde3vvused3h14", "etxf60u6r6ysvuy6", "uq0rpc2pbqa6hrvc", {
     port: 3306,
     host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
     pool: {
       max: 10,
-      // min: 0,
+      min: 0,
       idle: 30000
-    },
-    // dialectOptions: {
-    //     socketPath: "/var/run/mysqld/mysqld.sock"
-    // },
-    define: {
-        paranoid: true
     }
   });
-// }
+}
 
 // Exports the connection for other files to use
 module.exports = sequelize;
